8dcea99d187ee1fc7210ded9337dabb52cee7e79
JohnMichaelEmeterio <115387896+JohnMichaelEmeterio@users.noreply.github.com>
2023-05-08 23:20:59 +0800

    Step 2
    Completed

---
+++ doctor_appointment_app/lib/utils/text.dart
@@ -0,0 +1,12 @@
+class AppText {
+  static final enText = {
+    'welcome_text': 'Welcome',
+    'signIn_text': 'Sign in into your account',
+    'registered_text': 'Already have an Account?',
+    'register_text':
+        'You can easily sign up, and connect to the doctor nearby to you',
+    'signUp_text': 'Dont have an Account',
+    'social-login': 'Or continue with social account',
+    'forgot-password': 'Forgot your password?',
+  };
+}
