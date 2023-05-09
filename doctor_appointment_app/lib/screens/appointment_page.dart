bc62ca85369dee0f9bcbf4b5e2e4c8774ea3eb48
JohnMichaelEmeterio <115387896+JohnMichaelEmeterio@users.noreply.github.com>
2023-05-09 11:21:45 +0800

    done appointment page

--- doctor_appointment_app/lib/screens/appointment_page.dart
+++ doctor_appointment_app/lib/screens/appointment_page.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment_app/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentPage extends StatefulWidget {
@@ -7,11 +8,73 @@ class AppointmentPage extends StatefulWidget {
   State<AppointmentPage> createState() => _AppointmentPageState();
 }
 
+enum FilterStatus { upcoming, complete, cancel }
+
 class _AppointmentPageState extends State<AppointmentPage> {
+  FilterStatus status = FilterStatus.upcoming;
+  Alignment _alignment = Alignment.centerLeft;
+  List<dynamic> schedules = [
+    {
+      "doctor_name": "Richard Tan",
+      "doctor_profile": "assets/doctor_2.jpg",
+      "category": "Dental",
+      "status": FilterStatus.upcoming,
+    },
+    {
+      "doctor_name": "Max Lim",
+      "doctor_profile": "assets/doctor_3.jpg",
+      "category": "Cardiology",
+      "status": FilterStatus.complete,
+    },
+    {
+      "doctor_name": "Jane Wong",
+      "doctor_profile": "assets/doctor_4.jpg",
+      "category": "Respiratory",
+      "status": FilterStatus.complete,
+    },
+    {
+      "doctor_name": "Jenny Song",
+      "doctor_profile": "assets/doctor_5.jpg",
+      "category": "General",
+      "status": FilterStatus.cancel,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Appointment Page'),
+    List<dynamic> filteredShedules = schedules.where((var schedule) {
+      // switch (schedule['status']) {
+      //   case 'upcoming':
+      //     schedule['status'] = FilterStatus.upcoming;
+      //     break;
+      //   case 'complete':
+      //     schedule['status'] = FilterStatus.complete;
+      //     break;
+      //   case 'cancel':
+      //     schedule['status'] = FilterStatus.cancel;
+      //     break;
+      //  }
+      return schedule['status'] == status;
+    }).toList();
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const <Widget>[
+            Text(
+              'Appointment Schedule',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Config.spaceSmall,
+          ],
+        ),
+      ),
     );
   }
 }
