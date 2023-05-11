9e8a38f0b2483474cb278acdda0d20a943f6a693
JohnMichaelEmeterio <115387896+JohnMichaelEmeterio@users.noreply.github.com>
2023-05-11 09:28:52 +0800

    booking page is done

--- doctor_appointment_app/lib/screens/booking_page.dart
+++ doctor_appointment_app/lib/screens/booking_page.dart
@@ -55,6 +55,74 @@ class _BookingPageState extends State<BookingPage> {
                 )
               ],
             ),
+          ),
+          _isWeekend
+              ? SliverToBoxAdapter(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 30),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Weekend is not available, please select another date',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                )
+              : SliverGrid(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return InkWell(
+                        splashColor: Colors.transparent,
+                        onTap: () {
+                          setState(() {
+                            _currentIndex = index;
+                            _timeSelected = true;
+                          });
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: _currentIndex == index
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                            color: _currentIndex == index
+                                ? Config.primaryColor
+                                : null,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            '${index + 9}:00 ${index + 9 > 11 ? "PM" : "AM"}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color:
+                                  _currentIndex == index ? Colors.white : null,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: 8,
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4, childAspectRatio: 1.5),
+                ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
+              child: Button(
+                width: double.infinity,
+                title: 'Make Appointment',
+                onPressed: () {},
+                disable: _timeSelected && _dateSelected ? false : true,
+              ),
+            ),
           )
         ],
       ),
